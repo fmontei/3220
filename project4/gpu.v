336d189285020c66c3ba8c4e6d066edd25dab81f
fmontei <felipe.carneiro.monteiro@gmail.com>
2015-02-18 13:44:18 -0500

    Testing gpu.v

--- project4/gpu.v
+++ project4/gpu.v
@@ -46,28 +46,34 @@ reg [9:0]  colInd;
 reg init_phase; 
 
 /* Our registers */
-parameter x1 = 1;
-parameter x2 = 30;
-parameter y1 = 1;
-parameter y2 = 30;
+parameter x1 = 0;
+parameter x2 = 200;
+parameter y1 = 0;
+parameter y2 = 200;
 
+wire dy, dx;
 reg [31:0] x, y, x1_new, x2_new, y1_new, y2_new;
 reg [31:0] delta, offset, threshold, threshold_inc;
-reg signed [31:0] dy = y2 - y1, dx = x2 - x1, m;
+reg signed [31:0] m;
+reg [31:0] abs_dy, abs_dx;
 reg signed [2:0] adjust;
-reg [0:0] in_triangle;
 
-reg [0:0] pixels[y2 - y1 - 1:0][x2 - x1 - 1:0];
-reg [9:0] i, j;
+reg [0:0] in_triangle = 0;
+reg [31:0] i, j;
+
+assign dy = y2 - y1;
+assign dx = x2 - x1;
 
 initial begin	
-	//m  <= dy / dx;
+	
+	//abs_dy = (dy >= 0) ? dy : -dy;
+	//abs_dx = (dx >= 0) ? dx : -dx;
+	//adjust = ((dy >= 0 && dx >= 0) || (dy < 0 && dx < 0)) ? 1 : -1;
 	m = 1;
-	
 	adjust = (m >= 0) ? 1 : -1;
-	offset = 0;
+ 	offset = 0;
 	
-	if (m <= 1 && m >= -1) begin
+	if (/*abs_dy >= abs_dx*/ m >= -1 && m <= 1) begin
 		delta = (dy > 0) ? dy * 2 : dy * -2;
 		threshold = (dx > 0) ? dx : -dx;
 		threshold_inc = threshold * 2;
@@ -94,12 +100,6 @@ initial begin
 		end
 		y = y1_new;
 	end
-	
-	for (i = 0; i < y2 - y1; i = i + 1) begin
-		for (j = 0; j < x2 - x1; j = j + 1) begin
-			pixels[i][j] <= 0;
-		end
-	end
 end
 				
 always @(posedge I_CLK or negedge I_RST_N)
@@ -112,63 +112,53 @@ begin
 		count <= 0;
 	end else begin
 		if (!I_VIDEO_ON) begin
-		
 			/* In Triangle Logic */
-			if (m <= 1 && m >= -1) begin
-				if (x <= x2_new) begin
-					pixels[y][x] <= 1;
+			if (/*abs_dy >= abs_dx*/ m >= -1 && m <= 1) begin
+				if (x < x2_new) begin
 					offset <= offset + delta;
 					if (offset >= threshold) begin
 						y <= y + adjust;
 						threshold <= threshold + threshold_inc;
 					end
 					x <= x + 1;
+					in_triangle <= 1;
 				end else begin
-					in_triangle <= 0;
-					/*delta <= (dy > 0) ? dy * 2 : dy * -2;
+					x <= x1_new;
+					y <= (x2 < x1) ? y2 : y1;
+					offset <= 0;
 					threshold <= (dx > 0) ? dx : -dx;
-					threshold_inc <= threshold * 2;
-					x1_new <= x1;
-					x2_new <= x2;
-					y <= y1;
-					if (x2 < x1) begin
-						x1_new <= x2;
-						x2_new <= x1;
-						y <= y2;
-					end
-					x <= x1_new;*/
+					in_triangle <= 0;
 				end
-			end 
+			end else begin
+				if (y < y2_new) begin
+					offset <= offset + delta;
+					if (offset >= threshold) begin
+						x <= x + adjust;
+						threshold <= threshold + threshold_inc;
+					end
+					y <= y + 1;
+				end
+			end
 		
 		  	count <= count + 1;
 			if (count[26] == 0) begin			
-				O_GPU_ADDR <= rowInd * 640 + colInd;
+				O_GPU_ADDR <= x * 640 + y;
 				O_GPU_WRITE <= 1'b1;
 				O_GPU_READ <= 1'b0;
-				//if (/*rowInd == x && colInd == y &&*/ in_triangle == 1)
-					//O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};	
-				//else 
-					//O_GPU_DATA <= {4'h0, 4'h0, 4'h3, 4'hf};
-				if (rowInd >= x1 && rowInd <= x2 && colInd >= y1 && colInd <= y2)
-					O_GPU_DATA <= (pixels[colInd][rowInd] == 1) ? 
-						{4'hf, 4'hf, 4'hf, 4'hf} : {4'h0, 4'h0, 4'h3, 4'hf};	
-				else
-					O_GPU_DATA <= {4'h0, 4'h0, 4'h3, 4'hf};
+				if (in_triangle == 1 && rowInd == x && colInd == y)
+					O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};	
+				else 
+					O_GPU_DATA <= {4'h0, 4'h0, 4'hf, 4'hf};
 			end 
 			/* reset the screen */ 		
 			else if (count[26] == 1) begin 	
-				O_GPU_ADDR <= rowInd * 640 + colInd;
+				O_GPU_ADDR <= x * 640 + y;
 				O_GPU_WRITE <= 1'b1;
 				O_GPU_READ <= 1'b0;
-				//if (/*rowInd == x && colInd == y &&*/ in_triangle == 1)
-					//O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};
-				//else 
-					//O_GPU_DATA <= {4'h0, 4'h4, 4'h3, 4'h0};
-				if (rowInd >= x1 && rowInd <= x2 && colInd >= y1 && colInd <= y2)
-					O_GPU_DATA <= (pixels[colInd][rowInd] == 1) ? 
-						{4'hf, 4'hf, 4'hf, 4'hf} : {4'h3, 4'hf, 4'h0, 4'h0};
-				else
-					O_GPU_DATA <= {4'h3, 4'hf, 4'h0, 4'h0};
+				if (in_triangle == 1 && rowInd == x && colInd == y)
+					O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};	
+				else 
+					O_GPU_DATA <= {4'h0, 4'h4, 4'h3, 4'h0};
 			end 	
 		end 
 	end
