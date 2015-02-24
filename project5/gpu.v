d987ce614f75c688a7d750cc73b5f1c607dd473c
fmontei <felipe.carneiro.monteiro@gmail.com>
2015-02-23 19:37:44 -0500

    Removed unused code from gpu.v and combined changes into 1 archived project

--- project5/gpu.v
+++ project5/gpu.v
@@ -27,59 +27,48 @@ output reg         O_GPU_WRITE;
 output reg         O_GPU_READ;
 
 // 7-segment display interface 
-
 output [6:0] O_HEX0, O_HEX1, O_HEX2, O_HEX3;
-
-/////////////////////////////////////////
-// Example code goes here 
-//
-// ## Note
-// It is highly recommended to play with this example code first so that you
-// could get familiarized with a set of output ports. By doing so, you would
-// get the hang of dealing with vector (graphics) objects in pixel frame.
-/////////////////////////////////////////
 
 /* Provided registers */
 reg [26:0] count;
-reg [9:0]  rowInd;
-reg [9:0]  colInd;
-reg init_phase; 
 
 /* Our registers & wires */
-reg [10:0] ax, ay, bx, by, cx, cy;
+reg [10:0] ax[4:0], ay[4:0], bx[4:0], by[4:0], cx[4:0], cy[4:0];
 reg [10:0] x, y;
 reg [0:0] in_triangle;
 reg signed [31:0] e1;
 reg signed [31:0] e2;
 reg signed [31:0] e3;
+reg [2:0] i;
 
 wire [31:0] min_x, min_y, max_x, max_y;
-assign min_x = (ax <= bx && ax <= cx) ? ax :
-					(bx <= ax && bx <= cx) ? bx :
-					/*(cx <= ax && cx <= bx)*/ cx;
-assign min_y = (ay <= by && ay <= cy) ? ay :
-				   (by <= ay && by <= cy) ? by :
-				   /*(cy <= cx && cy <= by)*/ cy;		
-assign max_x = (ax >= bx && ax >= cx) ? ax :
-				   (bx >= ax && bx >= cx) ? bx :
-				   /*(cx >= ax && cx >= bx)*/ cx;
-assign max_y = (ay >= by && ay >= cy) ? ay :
-				   (by >= ay && by >= cy) ? by :
-				   /*(cy >= ay && cy >= by)*/ cy;
+assign min_x = (ax[i] <= bx[i] && ax[i] <= cx[i]) ? ax[i] :
+					(bx[i] <= ax[i] && bx[i] <= cx[i]) ? bx[i] :
+					/*(cx <= ax && cx <= bx)*/ cx[i];
+assign min_y = (ay[i] <= by[i] && ay[i] <= cy[i]) ? ay[i] :
+				   (by[i] <= ay[i] && by[i] <= cy[i]) ? by[i] :
+				   /*(cy <= cx && cy <= by)*/ cy[i];		
+assign max_x = (ax[i] >= bx[i] && ax[i] >= cx[i]) ? ax[i] :
+				   (bx[i] >= ax[i] && bx[i] >= cx[i]) ? bx[i] :
+				   /*(cx >= ax && cx >= bx)*/ cx[i];
+assign max_y = (ay[i] >= by[i] && ay[i] >= cy[i]) ? ay[i] :
+				   (by[i] >= ay[i] && by[i] >= cy[i]) ? by[i] :
+				   /*(cy >= ay && cy >= by)*/ cy[i];
 
 initial begin
 	/* test case triangle 1 */
-	ax = 1; ay = 1; bx = 200; by = 100; cx = 50; cy = 50;	
+	ax[0] = 1; ay[0] = 1; bx[0] = 200; by[0] = 100; cx[0] = 50; cy[0] = 50;	
 	/* test case triangle 2 */
-	//ax = 100; ay = 100; bx = 200; by = 100; cx = 200; cy = 200;	
+	ax[1] = 100; ay[1] = 100; bx[1] = 200; by[1] = 100; cx[1] = 200; cy[1] = 200;	
 	/* test case triangle 3 */
-	//ax = 100; ay = 100; bx = 200; by = 100; cx = 200; cy = 1;	
+	ax[2] = 100; ay[2] = 100; bx[2] = 200; by[2] = 100; cx[2] = 200; cy[2] = 1;	
 	/* test case triangle 4 */
-	//ax = 20; ay = 20; bx = 100; by = 1; cx = 1; cy = 1;	
+	ax[3] = 20; ay[3] = 20; bx[3] = 100; by[3] = 1; cx[3] = 1; cy[3] = 1;	
 	/* test case triangle 5 */
-	//ax = 1; ay = 1; bx = 100; by = 90; cx = 20; cy = 100;	
+	ax[4] = 1; ay[4] = 1; bx[4] = 100; by[4] = 90; cx[4] = 20; cy[4] = 100;	
 	x = 0;
 	y = 0;
+	i = 0;
 	in_triangle = 0;
 end
 				
@@ -91,6 +80,7 @@ begin
 		O_GPU_READ <= 1'b0;
 		O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};
 		count <= 0;
+		i <= 0;
 	end else begin
 		if (!I_VIDEO_ON) begin
 		
@@ -101,9 +91,9 @@ begin
 			if (y <= 639) begin
 				if (x <= 399) begin
 					if (x >= min_x && x <= max_x && y >= min_y && y <= max_y) begin
-						e1 <= (-(cy - by) * (x - bx)) + ((cx - bx) * (y - by));
-						e2 <= (-(ay - cy) * (x - cx)) + ((ax - cx) * (y - cy));
-						e3 <= (-(by - ay) * (x - ax)) + ((bx - ax) * (y - ay));
+						e1 <= (-(cy[i] - by[i]) * (x - bx[i])) + ((cx[i] - bx[i]) * (y - by[i]));
+						e2 <= (-(ay[i] - cy[i]) * (x - cx[i])) + ((ax[i] - cx[i]) * (y - cy[i]));
+						e3 <= (-(by[i] - ay[i]) * (x - ax[i])) + ((bx[i] - ax[i]) * (y - ay[i]));
 					end
 					/* After testing this in C code, it is necessary to check whether
 					 * all three edges are positive or all three are negative */
@@ -129,14 +119,12 @@ begin
 					O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};
 			end 
 			/* reset the screen */ 		
-			else if (count[24] == 1) begin 	
+			else if (count[24] == 1) begin 
 				O_GPU_ADDR <= x * 640 + y;
 				O_GPU_WRITE <= 1'b1;
 				O_GPU_READ <= 1'b0;
-				if (in_triangle == 1)
-					O_GPU_DATA <= {4'hf, 4'hf, 4'hf, 4'hf};
-				else 
-					O_GPU_DATA <= {4'h0, 4'h0, 4'h0, 4'h0};
+				O_GPU_DATA <= {4'h0, 4'h0, 4'h0, 4'h0};	
+				i <= (i + 1) % 5;
 			end 		
 		end 
 	end
@@ -146,35 +134,5 @@ SevenSeg sseg0(.IN(min_x[ 3: 0]),.OUT(O_HEX0));
 SevenSeg sseg1(.IN(min_x[ 7: 4]),.OUT(O_HEX1));
 SevenSeg sseg2(.IN(min_x[11: 8]),.OUT(O_HEX2));
 SevenSeg sseg3(.IN(min_x[15:12]),.OUT(O_HEX3));
-
-always @(posedge I_CLK or negedge I_RST_N)
-begin
-  if (!I_RST_N) begin
-    colInd <= 0;
-  end else begin
-    if (!I_VIDEO_ON) begin
-      if (colInd < 639)
-        colInd <= colInd + 1;
-      else
-        colInd <= 0;
-    end
-  end
-end
-
-always @(posedge I_CLK or negedge I_RST_N)
-begin
-  if (!I_RST_N) begin
-    rowInd <= 0;
-  end else begin
-    if (!I_VIDEO_ON) begin
-      if (colInd == 0) begin
-        if (rowInd < 399)
-          rowInd <= rowInd + 1;
-        else
-          rowInd <= 0;
-      end
-    end
-  end
-end
 
 endmodule
